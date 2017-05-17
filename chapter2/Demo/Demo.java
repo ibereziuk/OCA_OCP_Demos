d233391a48c82e7214e0e91ecacbeaf6733fe145
Ivan Bereziuk <ibereziuk@qualogy.com>
2017-05-17 16:20:02 +0200

    Set proper source placement

---
+++ chapter2/Demo/Demo.java
@@ -0,0 +1,96 @@
+import java.util.*;
+import java.util.function.*;
+
+public class Demo
+{	
+	public static void main(String[] args)
+	{
+		int nCows = args.length == 0 ? 10 : Integer.parseInt(args[0]);
+		
+		ArrayList<Cow> herdCow = createCowHerd(nCows);
+	
+		// print(herdCow);
+				
+		int nCollisions = calcCollisions(herdCow, (c) -> c);
+		int nHashCollisions = calcCollisions(herdCow, (c) -> c.hashCode());
+		
+		displayResults("Object", nCollisions, nCows);
+		displayResults("Hash", nHashCollisions, nCows);
+	}
+	
+	
+	private static ArrayList<Cow> createCowHerd(int nCows)
+	{
+		final int MIN_AGE = 0;
+		final int MAX_AGE = 6;
+		
+		String[] namesPool = {
+			"Bessie", "Clarabelle", "Betty Sue",
+			"Emma", "Henrietta", "Ella",
+			"Penelope", "Nettie", "Anna",
+			"Bella", "Annabelle", "Dorothy",
+			"Molly", "Gertie", "Annie"
+		};
+		
+		Random rand = new Random();
+		Cow.Color[] colors = Cow.Color.values();
+
+
+		ArrayList<Cow> herdCow = new ArrayList<>();
+		
+		int age;
+		String name;
+		Cow.Color color;
+		
+		for (int i = 0; i < nCows; i++)
+		{			
+			age = rand.nextInt(MAX_AGE + 1);
+			name = namesPool[rand.nextInt(namesPool.length)];
+			color = colors[rand.nextInt(colors.length)];
+			herdCow.add(new Cow(age, name, color));
+		}
+		
+		return herdCow;
+	}
+	
+	
+	private static void print(ArrayList<Cow> herd)
+	{
+		StringJoiner sj = new StringJoiner(",\n\t", "[\n\t", "\n]");
+		for(Cow c : herd)
+		{
+			sj.add(c.toString());
+		}
+		System.out.println(sj.toString());
+	}
+
+	
+	private interface ValueRetriever
+	{
+		Object retrieve(Object c);
+	}
+	
+	private static int calcCollisions(ArrayList<Cow> herd, ValueRetriever retriever)
+	{
+		Set<Object> cowHashSet = new HashSet<>();
+		int hashCollisions = 0;
+		for(Cow c : herd)
+		{
+			boolean success = cowHashSet.add(retriever.retrieve(c));
+			if(!success)
+			{
+				// System.out.println(c);
+				hashCollisions++;
+			}
+		}
+		return hashCollisions;
+	}
+	
+	
+	static void displayResults(String collisionType, int nCollisions, int nTotal)
+	{
+		System.out.println("\n\t" + collisionType +
+			String.format(" collision rate: %d out of %d = ", nCollisions, nTotal) 
+			+ (100 * (double)nCollisions / nTotal) + "%");
+	}
+}
